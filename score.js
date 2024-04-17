780ba3f1973a0d4cdde23410a23f1b003e7470bb
Miguel Silva <miguelklogsilva@gmail.com>
2024-04-17 21:54:20 +0100

    acabei a 2º entrega

--- score.js
+++ score.js
@@ -1,4 +1,3 @@
-// Definição da classe para scores
 class Score {
     constructor(nome, score) {
         this.nome = nome;
@@ -21,10 +20,10 @@ function add_score(new_score) {
         current_scores = [new_score]
         localStorage.setItem("Scores", JSON.stringify(current_scores))
     }
-    
-    // substituir nomes duplicados pelo que tem maior score
+
+    // substituir nomes duplicados pelo que tem maior score (ainda não funciona a 100%)
     for (var i = 0; i < current_scores.length; i++) {
-        if (new_score.nome == current_scores[i].nome && new_score.score >= current_scores[i].score) {
+        if (new_score.nome == current_scores[i].nome && new_score.score > current_scores[i].score) {
             console.log("replacing " + new_score.nome + " score")
             current_scores.splice(i, 1)
         }
@@ -35,14 +34,15 @@ function add_score(new_score) {
     localStorage.setItem("Scores", JSON.stringify(current_scores))
 }
 
+// alguns dados pre-feitos, depois será integrado no jogo
 localStorage.setItem("Scores", null)
-add_score(new Score("Miguel", 1))
-add_score(new Score("Johny", 100))
-add_score(new Score("Maria", 75))
+add_score(new Score("Miguel", 100))
 add_score(new Score("José", 500))
-add_score(new Score("José", 250))
-add_score(new Score("José", 1000))
+add_score(new Score("Profª Teresa Terroso", 99999))
 add_score(new Score("Spongebob", 10000))
-add_score(new Score("José", 250))
-add_score(new Score("José", 250))
+add_score(new Score("Bruna", 250))
+
+if (localStorage.getItem("Nome") != null) {
+    add_score(new Score(localStorage.getItem("Nome"), 10001))
+}
 
